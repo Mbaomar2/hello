1f7452c2e807ad818ed22a2084286b7bf4e8f636
Mohammed Ba Omar <qwert77817@gmail.com>
2019-08-29 13:00:49 -0500

    Create hello.c
    Made changes to the code

---
+++ hello.c
@@ -0,0 +1,15 @@
+/*
+Name: Mohammed Ba Omar
+Date: 8/27/2019
+Class: CSCE-155E
+Activity: Lab 1
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  printf(“Mohammed Ba Omar\n”);
+  printf(“Civil Engineering\n”);
+  return 0;
+}
