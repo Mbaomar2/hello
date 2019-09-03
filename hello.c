16c57c1501faf93b5dc070588d94e15feffc000d
cyoung-38 <54381275+cyoung-38@users.noreply.github.com>
2019-09-02 20:42:36 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -11,5 +11,7 @@ Activity: Hack 1
 int main(int argc, char *argv[]) {
   printf(“Mohammed Ba Omar\n”);
   printf(“Civil Engineering\n”);
+  printf(“Colby Young\n”);
+  printf(“Computer Engineering\n”);
   return 0;
 }
